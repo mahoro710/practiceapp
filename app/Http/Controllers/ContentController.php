c51acb70f802d2c65457690f628c773ab290a03a
mahoro710 <mahoro710@gmail.com>
2022-05-15 06:28:49 +0900

    　適切なコミットメント

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Content;
+use Illuminate\Http\Request;
+
+class ContentController extends Controller
+{
+
+public function mypage()
+    {
+        return view('contents/mypage');  
+    }
+}
