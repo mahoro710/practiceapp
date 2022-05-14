95610e70f572ed37313ba5192e9cf51d8dbf5167
mahoro710 <mahoro710@gmail.com>
2022-05-15 02:59:06 +0900

    適切なコミットメント

---
+++ resources/views/contents/mypage.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>mypage</title>
+
+    <body>
+        <h1>今日の記録</h1>
+        <p>家事の追加</p>
+        <p>ポイント確認</P>
+        <p>達成カレンダー</p>
+        
+    </body>
+</html>
