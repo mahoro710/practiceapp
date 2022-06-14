5cc9f86f793e01e262b18e04fb74b311b2d11651
mahoro710 <mahoro710@gmail.com>
2022-06-14 19:44:08 +0900

    適切なコミットメント

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,10 @@ Route::get('home/edit','HouseworkController@create');
 Route::get('home','HouseworkController@myhome');
 Route::get('home/point','HouseworkController@point');
 Route::get('home/calendar','HouseworkController@calendar');
-Route::post('home/index', 'HouseworkController@store');
+Route::post('home/index', 'houseworkController@store');
 Route::get('home/index','HouseworkController@index');
-Route::get('home/houseworks/{housework}', 'HouseworkController@show');
+Route::get('home/{housework}', 'HouseworkController@show');
+Route::delete('/home/{housework}', 'HouseworkController@delete');
 });
 
 
@@ -19,14 +20,7 @@ Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
-=======
-Route::get('/', 'ContentController@login');
-Route::get('/home','ContentController@home');
-Route::get('/home/edit','ContentController@edit');
-Route::get('/home/point','ContentController@point');
-Route::get('/home/calendar','ContentController@calendar');
+ 
 
 
 
->>>>>>> e64f7ac633400f6d181b7db1a54526faa48b604a
